9161ed33f547ed98cd1062504a9dd8bdaa242d39
Phonethip <phonethip.liu@gmail.com>
2018-01-22 09:07:53 -0600

    first commit

---
+++ coinsToCash.js
@@ -0,0 +1,12 @@
+// Define an object and store it in a variable named piggyBank. The object should have the following keys defined.
+
+// quarters
+// nickels
+// dimes
+// pennies
+// For each coin type, give yourself as many as you like.
+
+
+// I like pennies
+const piggyBank = { pennies: 342, nickels: 100, dimes: 50, quaters: 80 }
+let dollarAmount = 0
