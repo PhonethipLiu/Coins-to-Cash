a81088775ce4e8909f45497cc259eebb4bcbfb83
Phonethip <phonethip.liu@gmail.com>
2018-01-28 17:42:26 -0600

    finished code: got it to work

--- coinsToCash.js
+++ coinsToCash.js
@@ -6,7 +6,48 @@
 // pennies
 // For each coin type, give yourself as many as you like.
 
+// Once you have given yourself a large stash of coins in your piggybank, look at each key and perform the appropriate math on the integer value to determine how much money you have in dollars. Store that value in a variable named dollarAmount.
+
+// When done, output the value to the browser console.
+
+// For example, if my piggy bank only held those pennies from above, when I type the variable name of dolllarAmount into the console, the output would be.
+
+// > dollarAmount
+// $3.42
 
 // I like pennies
-const piggyBank = { pennies: 342, nickels: 100, dimes: 50, quaters: 80 }
-let dollarAmount = 0
+// const piggyBank = { pennies: 342, nickels: 100, dimes: 50, quarters: 80 }
+//Test to see math conversion for coins
+// console.log("$" + piggyBank.pennies / 100);
+
+const piggyBank = { pennies: 342, nickels: 100, dimes: 50, quarters: 80 };
+let dollarAmount = 0;
+
+let p = piggyBank.pennies / 100;
+let n = piggyBank.nickels / 20;
+let d = piggyBank.dimes / 10;
+let q = piggyBank.quarters / 4;
+
+dollarAmount = p + n + d + q;
+
+// console.log("$" + dollarAmount);
+
+document.getElementById("money").innerHTML = "You have <strong>$" + dollarAmount + "</strong> in your piggy bank.";
+
+
+// function bank() {
+//     for(i=0; i < piggyBank.length; i++){
+//         // var dollarAmount = 0;
+//         var p = piggyBank.pennies / 100;
+//         var n = piggyBank.nickels / 20;
+//         var d = piggyBank.dimes / 10;
+//         var q = piggyBank.quarter / 4;
+//         var dollarAmount = "$" + (p + n + d + q);
+//     }
+//     console.log(dollarAmount);
+// };
+// console.log(bank(p));
+// document.getElementById("money").innerHTML = bank(piggyBank);
+
+// console.log(bank);
+
